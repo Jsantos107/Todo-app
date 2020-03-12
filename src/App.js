903017df81b9f9ea14fe36d987071752a470bd76
github username <github email address>
2020-03-12 10:14:45 -0600

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state ={
+    todo:[
+      {id:1, title: "First Todo", content: "I have something todo"},
+      {id:1, title: "Second Todo", content: "I have some more todo"},
+      {id:1, title: "Third Todo", content: "I have too much todo"}
+    ]
+  }
+
+  render (){
+    return (
+      <div className="App">
+        
+      </div>
+    );
+  }
 }
 
 export default App;
